e2a2ce21d02d75e1493626e330c985804a1eb649
SnChul <104672461+SnChul@users.noreply.github.com>
2022-05-01 12:18:18 +0900

    [Quiz] 3 사이트 비밀번호

--- Quiz.py
+++ Quiz.py
@@ -3,6 +3,46 @@ station = '사당'
 print(station+'행 열차가 들어오고 있습니다.')
 
 #퀴즈 2 랜덤을 이용한 날짜 출력
+from dataclasses import replace
 from random import*
+from traceback import print_tb
 date = randint(4,28)
 print("오프라인 스터디 모임 날짜는 매월 " +str(date)+"일로 선정되었습니다.") 
+
+#퀴즈 3 사이트별 비밀번호
+site = "http://naver.com"
+site = "http://daum.com"
+# start = site.index("/")+2
+# end = site.index(".")
+# home = site[start:end]
+# count = len(home)
+# count2 = home.count("e")
+# pw = home[:3] + str(count) + str(count2)
+# print(site[start:end])
+# print(site[start:])
+# print(count2)
+# # print(count)
+# print(pw)
+
+site = site.replace("http://", "")
+site = site[:site.index(".")]
+print(site)
+password = site[:3] + str(len(site)) + str(site.count("e"))+"!"
+print(password)
+print("{0}의 비밀번호는 {1}입니다".format(site, password))
+print("%s의 비밀번호는 %s입니다" % (site,  password))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- StudyCode2.py
+++ StudyCode2.py
@@ -1,7 +1,9 @@
+
 from imp import IMP_HOOK
 from lib2to3.pgen2.token import RPAR
 from pickletools import read_int4
 from re import A
+from traceback import print_tb
 
 
 # 연산자
@@ -57,6 +59,84 @@ print(int(random()*10)+1) #1부터 10이하의 임의값
 print(int(random()*45)+1)
 print((randrange(1,46))) #1부터 46 미만의 임의값
 print(randint(1,45 )) #1부터 45이하의 임의값
+
+#문자열
+sentence = '나는 소년입니다'
+print(sentence)
+sentence = "파이썬은 쉬워요"
+print(sentence)
+a='''나는 소년이고 
+파이썬은 쉬워요'''
+print(a)
+
+#슬라이싱
+num = "990020-8234567"
+print("성별 : "+num[7])
+print("연 : "+num[0:2]) #0부터 2직전까지
+print("월 : "+num[2:4]) 
+print("생년월일 : "+num[:6]) #처음부터 6직전까지
+print("뒷자리 : "+num[7:])
+print("뒤에서부터 " +num[-7:])
+
+#문자열 처리함수
+a = "Python is Amazing"
+print(a.lower())
+print(a.upper())
+print(a[0].isupper()) #대문자가 맞으면 true
+print(len(a))         #길이  
+print(a.replace("Python", "Java")) #문자열 치환
+index = a.index("n") # 해당 문자 위치 
+print(index)
+index = a.index("n", index +1 ) #해당 문자 첫위치 다음위치
+print(index)
+
+print(a.find("Java")) # 원하는 값 없을 경우 -1 반환
+# print(a.index("Java"))  에러처리
+print(a.count("n")) #해당 글자 갯수
+
+#문자열 포맷
+print("a"+ "a")
+print("A"+"b")
+    #정수 문자열 문자 %d %s %c
+    #방법 1
+print("나는 %d살입니다" % 20)
+print("나는 %s을 좋아해요" % "파이썬")
+print("Apple 은 %c로 시작해요" % "A")
+print("나는 %s살입니다" % 20) #%s는 만능인가
+print("나는 %s과 %s을 좋아해요" % ("빨간색" , "파란색"))
+    #방법 2 
+print("나는 {}살 입니다.".format(20))
+print("나는 {}색과 {}색을 좋아해요".format("파란", "빨간") )
+print("나는 {1}색과 {0}색을 좋아해요".format("파란","빨간"))
+    #방법 3
+print("나는 {age}살이며, {color}색을 좋아해요".format(age=20, color="빨간색"))
+    #방법 4
+age = 20
+color = "빨간" #f는 기존 변수값을 가져와 사용한다
+print(f"나는 {age}살이며, {color}색을 좋아해요")
+
+#탈출문자
+print("문장을 줄바꿈하려면\n이렇게합니다")
+print('저는 \"SnChul\" 입니다') #\" 문자로 그대로 출력 \"
+    #문장 내에서 \를 출력하려면 \\
+print("C:\\Users\\we202\\Documents\\GitHub\\Python_project")
+    #\r 커서 맨 앞으로 이동
+print("Red Apple\rPine")
+    #\b 백스페이스 (한글자 삭제)
+print("REdd\bApple")
+    #\t tab 기능
+print("REd\tApple")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
